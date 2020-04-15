bfccdca8859ed5439808d3257ca5cd8bba2af1d9
RDxR10 <risirajdey@gmail.com>
2020-04-15 14:15:42 +0530

    Update PY_AUDIO.py

--- PY_AUDIO.py
+++ PY_AUDIO.py
@@ -1,8 +1,14 @@
 import speech_recognition as sr
-r=sr.Recognizer()
-with sr.Microphone() as source:
-  audio=r.listen(source)
-try:
-  print("system predicts:"+r.recognize_google(audio))
-except Exception:
-  print("something meant wrong");
+r=sr.recognizer
+while True:
+  r=sr.Recognizer()
+  with sr.Microphone() as source:
+    print("Say something")
+    audio=r.listen(source)
+    print(Done!)
+    try:
+      text = r.recognize_google(audio, language = 'en-US')
+      print(text)
+      print(r.recognize_google(audio))
+    except:
+      print("something meant wrong");
